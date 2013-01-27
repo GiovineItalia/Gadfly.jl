674676a7b47f72e92ff44e4fe2b5bcd061f8deff
Daniel Jones <dcjones@cs.washington.edu>
2013-01-26 17:20:34 -0800

    More work on mouseovers.

--- src/dynamics.js
+++ src/dynamics.js
@@ -34,6 +34,7 @@ function show_annotation(id)
 // Turn off a possibly hidden annotation.
 function hide_annotation(id)
 {
+
     annot = document.getElementById(id)
     annot.setAttribute('visibility', 'hidden')
 }
@@ -42,21 +43,67 @@ function hide_annotation(id)
 // Dim all geometry objects with some exceptions.
 function present_geometry(ids)
 {
+    on_anim_dur = 0.1
+
+    off_anim = document.getElementById('panel-focus-filter-off-anim')
+    off_anim.endElement()
+
+    t0 = Number(off_anim.getStartTime())
+    if (t0 < Infinity) {
+        t = Number(off_anim.getCurrentTime())
+        d = Number(off_anim.getSimpleDuration())
+        from = Number(off_anim.getAttribute('from'))
+        to = Number(off_anim.getAttribute('to'))
+        p = Math.max(0.0, Math.min(1.0, ((t - t0) / d)))
+        v = from + p * (to - from)
+    }
+    else {
+        p=1.0
+        v=0.0
+    }
+
     geoms = document.getElementsByClassName('geometry');
     for (i = 0; i < geoms.length; ++i) {
         geoms[i].setAttribute('filter', 'url(#panel-focus-filter)');
     }
 
     for (i = 0; i < ids.length; ++i) {
-        console.info(ids[i]);
         geom = document.getElementById(ids[i]);
         geom.setAttribute('filter', 'inherit');
     }
+
+    on_anim = document.getElementById('panel-focus-filter-on-anim')
+    on_anim.setAttribute('from', v)
+    on_anim.setAttribute('dur',  p * on_anim_dur + 's')
+    on_anim.beginElement()
 }
 
 
 // Set all geometries to full opacity.
 function unpresent_geometry()
+{
+    off_anim_dur = 0.3
+
+    on_anim = document.getElementById('panel-focus-filter-on-anim')
+    on_anim.endElement()
+
+    t0 = Number(on_anim.getStartTime())
+    t = Number(on_anim.getCurrentTime())
+    d = Number(on_anim.getSimpleDuration())
+    from = Number(on_anim.getAttribute('from'))
+    to = Number(on_anim.getAttribute('to'))
+    p = Math.max(0.0, Math.min(1.0, ((t - t0) / d)))
+    v = from + p * (to - from)
+
+    off_anim = document.getElementById('panel-focus-filter-off-anim')
+    off_anim.setAttribute('from', v)
+    off_anim.setAttribute('dur',  p * off_anim_dur + 's')
+    off_anim.beginElement()
+}
+
+
+// Called when the animation in unpresent_geometry finishes
+function unpresent_geometry_end()
 {
     geoms = document.getElementsByClassName('geometry');
     for (i = 0; i < geoms.length; ++i) {
