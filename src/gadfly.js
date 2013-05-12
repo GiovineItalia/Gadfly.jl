56249a1bfe99a611ab64effd2e0d0e3d16b33c33
Daniel Jones <dcjones@cs.washington.edu>
2013-05-12 09:25:05 -0700

    Set limits on panning.

--- src/gadfly.js
+++ src/gadfly.js
@@ -32,8 +32,8 @@ var guide_toggle_color = function(colorclass) {
               .style("opacity", 1.0);
             visible = true;
         }
-    })
-}
+    });
+};
 
 
 // Construct a callback used to toggle highly-visibility grid lines.
@@ -51,7 +51,7 @@ var guide_background_mouseover = function(color) {
           .duration(250)
           .attr("stroke", color);
     });
-}
+};
 
 var guide_background_mouseout = function(color) {
     return (function () {
@@ -60,7 +60,7 @@ var guide_background_mouseout = function(color) {
           .duration(250)
           .attr("stroke", color);
     });
-}
+};
 
 
 // Construct a call back used for mouseover effects in the point geometry.
@@ -78,41 +78,38 @@ var geom_point_mouseover = function(lw) {
           .duration(100)
           .attr("stroke-width", lw);
     });
-}
+};
 
 
-
-
+// Construct a callback used for dragbehavior.
+//
+// Args:
+//   t: A transform of the form {"x": x, "y": x} to close arround.
+//
+// Returns:
+//   A drag behavior.
+//
 var drag_behavior = function(t) {
     return d3.behavior.drag()
       .on("drag", function(d, i) {
-        t.x += d3.event.dx;
-        t.y += d3.event.dy;
+        var bbox = d3.select(".guide.background").select("path").node().getBBox();
+        t.x = Math.max(Math.min(t.x + d3.event.dx, bbox.width), -bbox.width);
+        t.y = Math.max(Math.min(t.y + d3.event.dy, bbox.height), -bbox.height);
 
         d3.selectAll(".geometry")
           .attr("transform", function() {
               return "translate(" + [t.x, t.y] + ")";
           });
 
-          d3.selectAll(".xgridlines,.xlabels")
-            .attr("transform", function() {
-                return "translate(" + [t.x, 0.0] + ")";
-            });
+        d3.selectAll(".xgridlines,.xlabels")
+        .attr("transform", function() {
+            return "translate(" + [t.x, 0.0] + ")";
+        });
 
-          d3.selectAll(".ygridlines,.ylabels")
-            .attr("transform", function() {
-                return "translate(" + [0.0, t.y] + ")";
-            });
-
-        // TODO:
-        // Clip tick labels.
-        //  How?
-        //  This would be nicer if we just added a Clip boolean to the canvas
-        //  constructor. Then any canvas could clip its contents. Ok. Let's do
-        //  that.
-        //
-        // Off-screen grid lines.
-        // Pan limits.
+        d3.selectAll(".ygridlines,.ylabels")
+          .attr("transform", function() {
+              return "translate(" + [0.0, t.y] + ")";
+          });
     });
-}
+};
 
