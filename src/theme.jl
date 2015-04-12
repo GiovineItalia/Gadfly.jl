b2068f468ff3ef56a740b3f23d25ed78db004158
Art Wild <wildart@gmail.com>
2015-04-12 12:50:40 -0400

    make number columns in key (legend) as a Theme parameter

--- src/theme.jl
+++ src/theme.jl
@@ -187,6 +187,9 @@ end
     # Probability of proposing a visibility flip during label layout.
     label_visibility_flip_pr,    Float64,   0.2
 
+    # Number of columns in key
+    key_max_columns,             Int,       4
+
 end
 
 
