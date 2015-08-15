cd9afaa6f61f1b1c7f9824fce9503e842c42ad4d
Joey Huchette <joehuchette@gmail.com>
2015-08-15 14:25:49 -0400

    fix for errorbar test

--- test/errorbar.jl
+++ test/errorbar.jl
@@ -8,10 +8,8 @@ n = 10
 ys = [mean(rand(Normal(0, sd), n)) for sd in sds]
 ymins = ys .- (1.96 * sds / sqrt(n))
 ymaxs = ys .+ (1.96 * sds / sqrt(n))
-cs = [string(i) for i in 1:n]
+cs = [string(i) for i in 1:length(sds)]
 
 plot(x=1:length(sds),
     x=ys, xmin=ymins, xmax=ymaxs,
     y=ys, ymin=ymins, ymax=ymaxs, color=cs, Geom.point, Geom.errorbar)
-
-
