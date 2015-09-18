9b9767c0a5e04a467e5325d1559c2f3bc3f918ea
Thomas Breloff <tom@breloff.com>
2015-09-17 23:33:45 -0400

    shapes: cross, xcross, utriangle, dtriangle, star1, star2, hexagon

--- src/shapes.jl
+++ src/shapes.jl
@@ -39,23 +39,143 @@ function diamond(xs::AbstractArray, ys::AbstractArray, rs::AbstractArray)
 end
 
 
+
 function cross(xs::AbstractArray, ys::AbstractArray, rs::AbstractArray)
-    n = max(length(xs), length(ys), length(rs))
+  n = max(length(xs), length(ys), length(rs))
+  polys = Vector{Vector{Tuple{Measure, Measure}}}(n)
+  for i in 1:n
+    x = x_measure(xs[mod1(i, length(xs))])
+    y = y_measure(ys[mod1(i, length(ys))])
+    r = rs[mod1(i, length(rs))]
+    u = 0.4r
 
-    polys = Vector{Vector{Tuple{Measure, Measure}}}(n)
-    s = 1/sqrt(5)
-    for i in 1:n
-        x = x_measure(xs[1 + i % length(xs)])
-        y = y_measure(ys[1 + i % length(ys)])
-        r = rs[1 + i % length(rs)]
-        u = s*r
-        polys[i] = Tuple{Measure, Measure}[
-            (x, y - u), (x + u, y - 2u), (x + 2u, y - u),
-            (x + u, y), (x + 2u, y + u), (x + u, y + 2u),
-            (x, y + u), (x - u, y + 2u), (x - 2u, y + u),
-            (x - u, y), (x - 2u, y - u), (x - u, y - 2u) ]
-    end
+    # make a "plus sign"
+    polys[i] = Tuple{Measure, Measure}[
+      (x-r, y-u), (x-r, y+u), # L edge
+      (x-u, y+u),             # BL inside
+      (x-u, y+r), (x+u, y+r), # B edge
+      (x+u, y+u),             # BR inside
+      (x+r, y+u), (x+r, y-u), # R edge
+      (x+u, y-u),             # TR inside
+      (x+u, y-r), (x-u, y-r), # T edge
+      (x-u, y-u)              # TL inside
+    ]
+  end
 
-    return polygon(polys)
+  return polygon(polys)
+end
+
+function xcross(xs::AbstractArray, ys::AbstractArray, rs::AbstractArray)
+  n = max(length(xs), length(ys), length(rs))
+  polys = Vector{Vector{Tuple{Measure, Measure}}}(n)
+  s = 1/sqrt(5)
+  for i in 1:n
+    x = x_measure(xs[mod1(i, length(xs))])
+    y = y_measure(ys[mod1(i, length(ys))])
+    r = rs[mod1(i, length(rs))]
+    u = s*r
+    polys[i] = Tuple{Measure, Measure}[
+      (x, y - u), (x + u, y - 2u), (x + 2u, y - u),
+      (x + u, y), (x + 2u, y + u), (x + u, y + 2u),
+      (x, y + u), (x - u, y + 2u), (x - 2u, y + u),
+      (x - u, y), (x - 2u, y - u), (x - u, y - 2u)
+    ]
+  end
+
+  return polygon(polys)
+end
+
+
+function utriangle(xs::AbstractArray, ys::AbstractArray, rs::AbstractArray, scalar = 1)
+  n = max(length(xs), length(ys), length(rs))
+  polys = Vector{Vector{Tuple{Measure, Measure}}}(n)
+  s = 0.8
+  for i in 1:n
+    x = x_measure(xs[mod1(i, length(xs))])
+    y = y_measure(ys[mod1(i, length(ys))])
+    r = rs[mod1(i, length(rs))]
+    u = 0.8 * scalar * r
+    polys[i] = Tuple{Measure, Measure}[
+      (x - r, y + u),
+      (x + r, y + u),
+      (x, y - u)
+    ]
+  end
+
+  return polygon(polys)
+end
+
+
+function dtriangle(xs::AbstractArray, ys::AbstractArray, rs::AbstractArray)
+    utriangle(xs, ys, rs, -1)
+end
+
+
+function star1(xs::AbstractArray, ys::AbstractArray, rs::AbstractArray, scalar = 1)
+  n = max(length(xs), length(ys), length(rs))
+  polys = Vector{Vector{Tuple{Measure, Measure}}}(n)
+  
+  # some magic scalars
+  sx = 0.7
+  sy1, sy2 = 1.2, 0.4
+
+  for i in 1:n
+    x = x_measure(xs[mod1(i, length(xs))])
+    y = y_measure(ys[mod1(i, length(ys))])
+    r = rs[mod1(i, length(rs))]
+    polys[i] = Tuple{Measure, Measure}[
+      (x-sx*r, y+r),      # BL
+      (x,      y-sy1*r),  # T
+      (x+sx*r, y+r),      # BR
+      (x-r,    y-sy2*r),  # L
+      (x+r,    y-sy2*r)   # R
+    ]
+  end
+
+  return polygon(polys)
+end
+
+
+
+function star2(xs::AbstractArray, ys::AbstractArray, rs::AbstractArray, scalar = 1)
+  n = max(length(xs), length(ys), length(rs))
+  polys = Vector{Vector{Tuple{Measure, Measure}}}(n)
+  for i in 1:n
+    x = x_measure(xs[mod1(i, length(xs))])
+    y = y_measure(ys[mod1(i, length(ys))])
+    r = rs[mod1(i, length(rs))]
+    u = 0.4r
+    polys[i] = Tuple{Measure, Measure}[
+      (x-u, y),   (x-r, y-r), # TL
+      (x,   y-u), (x+r, y-r), # TR
+      (x+u, y),   (x+r, y+r), # BR
+      (x,   y+u), (x-r, y+r)  # BL
+    ]
+  end
+
+  return polygon(polys)
+end
+
+
+function hexagon(xs::AbstractArray, ys::AbstractArray, rs::AbstractArray)
+  n = max(length(xs), length(ys), length(rs))
+
+  polys = Vector{Vector{Tuple{Measure, Measure}}}(n)
+  for i in 1:n
+    x = x_measure(xs[mod1(i, length(xs))])
+    y = y_measure(ys[mod1(i, length(ys))])
+    r = rs[mod1(i, length(rs))]
+    u = 0.4r
+
+    # make a "plus sign"
+    polys[i] = Tuple{Measure, Measure}[
+      (x-r, y-u), (x-r, y+u), # L edge
+      (x-u, y+r), (x+u, y+r), # B edge
+      (x+r, y+u), (x+r, y-u), # R edge
+      (x+u, y-r), (x-u, y-r)  # T edge
+    ]
+  end
+
+  return polygon(polys)
 end
 
