67e93ae16a4b1a1f9f27c4dfb15e20535e0cb17f
Daniel Jones <dcjones@cs.washington.edu>
2015-10-22 22:38:58 -0700

    Add precompile regen script.

---
+++ test/regen-precompiles.jl
@@ -0,0 +1,25 @@
+#!/usr/bin/env julia
+
+using SnoopCompile
+
+SnoopCompile.@snoop "gadfly_compiles.csv" begin
+    include("runtests.jl")
+end
+
+using Gadfly
+
+blacklist = ["__init__", "Image",
+             "unpack_pango_attr_list",
+             "newsurface",
+             "PangoAttr",
+             "update_pango_attr",
+             "pango_fmt_float",
+             "unpack_pango_attr",
+             "pango_text_extents",
+             "cairo_linecap",
+             "cairo_linejoin"]
+
+data = SnoopCompile.read("gadfly_compiles.csv")
+pc, discards = SnoopCompile.parcel(data[end:-1:1,2], blacklist=blacklist)
+SnoopCompile.write("snoop", pc)
+
