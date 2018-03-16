077f88cabc7bbe562713cab319b9412f66aacefd
Mattriks <Mattriks@users.noreply.github.com>
2018-03-17 03:06:59 +1100

    grid_line_style

--- test/testscripts/grid_line_style.jl
+++ test/testscripts/grid_line_style.jl
@@ -3,5 +3,5 @@ using Gadfly, DataArrays, RDatasets
 set_default_plot_size(6inch, 3inch)
 
 plot(dataset("datasets", "iris"), x="SepalLength", y="SepalWidth",
-     Theme(grid_strokedash=[]),
+     Theme(grid_line_style=:solid),
      Geom.point)
