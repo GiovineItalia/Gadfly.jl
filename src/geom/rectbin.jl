78170c1be4b823fd2952704dc23b5c5883c3164e
Ciarán O’Mara <Ciaran.OMara@utas.edu.au>
2018-10-26 16:53:58 +0200

    Possible render cascade

--- src/geom/rectbin.jl
+++ src/geom/rectbin.jl
@@ -64,56 +64,12 @@ element_aesthetics(::RectangularBinGeometry) =
 #
 function render(geom::RectangularBinGeometry, theme::Gadfly.Theme, aes::Gadfly.Aesthetics)
 
-    default_aes = Gadfly.Aesthetics()
-    default_aes.color = discretize_make_ia(RGBA{Float32}[theme.default_color])
-    aes = inherit(aes, default_aes)
-
     Gadfly.assert_aesthetics_defined("RectangularBinGeometry", aes, :xmin, :xmax, :ymin, :ymax)
     Gadfly.assert_aesthetics_equal_length("RectangularBinGeometry", aes, :xmin, :xmax, :ymin, :ymax)
 
-    nx = length(aes.xmin)
-    ny = length(aes.ymin)
-    n = nx
+    aes.xmax = x_measure(aes.xmax) .- theme.bar_spacing
 
-    xmin = aes.xmin
-    xwidths = [(x1 - x0)*cx - theme.bar_spacing
-               for (x0, x1) in zip(aes.xmin, aes.xmax)]
+    aes.ymax = y_measure(aes.ymax) .- theme.bar_spacing
 
-    ymin = aes.ymin
-    ywidths = [(y1 - y0)*cy - theme.bar_spacing
-               for (y0, y1) in zip(aes.ymin, aes.ymax)]
-
-    if length(aes.color) == n
-        cs = aes.color
-    else
-        cs = Array{RGBA{Float32}}(undef, n)
-        for i in 1:n
-            cs[i] = aes.color[((i - 1) % length(aes.color)) + 1]
-        end
-    end
-
-    allvisible = true
-    for c in cs
-        if c == nothing
-            allvisible = false
-            break
-        end
-    end
-
-    if !allvisible
-        visibility = cs .!= nothing
-        cs = cs[visibility]
-        xmin = xmin[visibility]
-        xmax = xmax[visibility]
-        ymin = ymin[visibility]
-        ymax = ymax[visibility]
-    end
-
-    return compose!(
-        context(),
-        rectangle(xmin, ymin, xwidths, ywidths, geom.tag),
-        fill(cs),
-        stroke(nothing),
-        svgclass("geometry"),
-        svgattribute("shape-rendering", "crispEdges"))
+    return render(RectangularGeometry(geom.default_statistic, geom.tag), theme, aes)
 end
