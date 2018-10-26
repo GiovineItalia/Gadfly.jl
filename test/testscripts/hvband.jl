3d6f4453e75c469aeec261cdaf6bf3e87a256fd8
Ciarán O’Mara <Ciaran.OMara@utas.edu.au>
2018-10-26 16:43:42 +0200

    Simplify test case

--- test/testscripts/hvband.jl
+++ test/testscripts/hvband.jl
@@ -1,21 +1,10 @@
-using Colors, Gadfly, RDatasets
+using Gadfly
 set_default_plot_size(21cm, 8cm)
 
-Dp = dataset("ggplot2","presidential")[3:end,:]
-De = dataset("ggplot2","economics")
-De[:Unemploy] /= 10^3
-
-p0 = plot(De, x=:Date, y=:Unemploy, Geom.line,
-            layer(Dp, xmin=:Start, xmax=:End, Geom.vband, color=:Party),
-            Scale.color_discrete_manual("deepskyblue", "lightcoral"),
-            Coord.cartesian(xmin=Date("1965-01-01"), ymax=12),
-            Guide.xlabel("Time"), Guide.ylabel("Unemployment (x10³)"), Guide.colorkey(title=""),
-            Theme(default_color="black", key_position=:top));
-
-p1 = plot(dataset("datasets", "iris"), x="SepalLength", y="SepalWidth", Geom.point,
+p1 = plot(x=collect(1:9), y=collect(1:9), Geom.point,
           layer(xmin=[5.0, 7.0], xmax=[6.5, 8.0] , Geom.vband, Theme(default_color="green")));
 
-p2 = plot(dataset("datasets", "iris"), x="SepalLength", y="SepalWidth", Geom.point,
+p2 = plot(x=collect(1:9), y=collect(1:9), Geom.point,
           layer(ymin=[2.5, 3.6], ymax=[3.0, 4.0], Geom.hband, Theme(default_color="red")));
 
-hstack(p0, p1, p2)
+hstack(p1, p2)
