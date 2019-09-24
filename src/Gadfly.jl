594a6b17ab5bd0ee2af11bc3f5e02b0808bc8776
Julien Jerphanion (@jjerphan) <git@jjerphan.xyz>
2019-09-24 09:18:58 +0200

    Update MIME type for show

--- src/Gadfly.jl
+++ src/Gadfly.jl
@@ -956,7 +956,7 @@ function show(io::IO, m::MIME"image/svg+xml", p::Plot)
     show(io, m, svg)
 end
 
-function show(io::IO, m::MIME"application/juno+plotpane", p::Plot)
+function show(io::IO, m::MIME"application/prs.juno.plotpane+html", p::Plot)
     buf = IOBuffer()
     svg = SVGJS(buf, Compose.default_graphic_width,
                 Compose.default_graphic_height, false)
