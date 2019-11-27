032b169838310d49d77b7bc49397a7b5515c47c8
Ciarán O'Mara <Ciaran.OMara@utas.edu.au>
2019-11-28 09:30:06 +1100

    Expand hvband test
    Covers the situation where hvband is put through the function apply_statistic_typed.

--- test/testscripts/hvband.jl
+++ test/testscripts/hvband.jl
@@ -5,4 +5,9 @@ p1 = plot(xmin=[1.0, 5.0, 7.0], xmax=[2.0, 6.5, 8.0] , Geom.vband, Theme(default
 
 p2 = plot(ymin=[2.5], ymax=[7.5], Geom.hband, Theme(default_color="red"));
 
-hstack(p1, p2)
+p3 = plot(
+    layer(Geom.band, xmin=[.25], xmax=[.75], Theme(default_color="red")),
+    layer(Geom.rect, xmin=[0], xmax=[1], ymin=[0], ymax=[1], Theme(default_color="blue"))
+) # Note: Covers function apply_statistic_typed.
+
+hstack(p1, p2, p3)
