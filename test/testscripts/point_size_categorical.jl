5f26c7b957dbdc737d7bb6597dc7aca7bce51075
Mattriks <Mattriks@users.noreply.github.com>
2019-12-28 20:01:23 +1100

    update test point_size_categorical

--- test/testscripts/point_size_categorical.jl
+++ test/testscripts/point_size_categorical.jl
@@ -1,5 +1,9 @@
 using Gadfly
 
-set_default_plot_size(6inch, 6inch)
+set_default_plot_size(6inch, 3inch)
 
-plot(x=rand(100), y=rand(100), size=rand(["foo","bar","pooh"], 100), Geom.point)
+x, y, size = rand(10), rand(10), rand(["foo","bar","pooh"], 10)
+p1 = plot(x=x, y=y, size=size, Geom.point)
+p2 = plot(x=x, y=y, size=size, Geom.point, Scale.size_discrete2)
+
+hstack(p1, p2)
