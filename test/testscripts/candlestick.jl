cbda2bb06266d3ebe85b1b00c15615945a9bc7e5
Iblis Lin <iblis@hs.ntnu.edu.tw>
2020-03-27 20:45:35 +0800

    add testscripts

---
+++ test/testscripts/candlestick.jl
@@ -0,0 +1,12 @@
+using Gadfly, MarketData
+
+set_default_plot_size(1920px, 1080px)
+
+ta = ohlc[1:100]
+plot(
+    x     = timestamp(ta),
+    open  = values(ta.Open),
+    high  = values(ta.High),
+    low   = values(ta.Low),
+    close = values(ta.Close),
+    Geom.candlestick)
