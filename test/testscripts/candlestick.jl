f40095f0fb89c1588d312a66e33315f3cea783e0
Iblis Lin <iblis@hs.ntnu.edu.tw>
2020-03-30 18:02:57 +0800

    testscripts

--- test/testscripts/candlestick.jl
+++ test/testscripts/candlestick.jl
@@ -3,10 +3,21 @@ using Gadfly, MarketData
 set_default_plot_size(1920px, 1080px)
 
 ta = ohlc[1:50]
-plot(
+p1 = plot(
     x     = timestamp(ta),
     open  = values(ta.Open),
     high  = values(ta.High),
     low   = values(ta.Low),
     close = values(ta.Close),
     Geom.candlestick)
+
+p2 = plot(
+    x     = timestamp(ta),
+    open  = values(ta.Open),
+    high  = values(ta.High),
+    low   = values(ta.Low),
+    close = values(ta.Close),
+    Geom.candlestick,
+    Scale.color_discrete_manual("green", "red"))
+
+hstack(p1, p2)
