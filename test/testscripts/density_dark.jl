ea5b56a7cafaaa4787d53392ae0e2e0a1b7c6ab2
Mattriks <Mattriks@users.noreply.github.com>
2020-05-28 04:39:53 +1000

    update add_arg_table, hex and winsor

--- test/testscripts/density_dark.jl
+++ test/testscripts/density_dark.jl
@@ -1,4 +1,4 @@
-using Gadfly, RDatasets, Test, Base64
+using Gadfly, RDatasets, Test, Base64, Colors
 
 set_default_plot_size(6inch, 3inch)
 
@@ -8,14 +8,14 @@ p = Gadfly.with_theme(:dark) do
 end
 
     svg_str_dark = stringmime(MIME("image/svg+xml"), p)
-    @test occursin(Base.hex(Gadfly.dark_theme.default_color), svg_str_dark)
+    @test occursin(Colors.hex(Gadfly.dark_theme.default_color), svg_str_dark)
 #    @test occursin("rgba(34,40,48,1)", svg_str_dark) # dark theme background color
 #    @test occursin("rgba(34,40,48,1)", svg_str_dark) # dark theme panel fill
 
     # Test reset.
     p2 = plot(dataset("ggplot2", "diamonds"), x="Price", color="Cut", Geom.density)
     svg_str_light = stringmime(MIME("image/svg+xml"), p2)
-    @test !occursin(Base.hex(Gadfly.dark_theme.default_color), svg_str_light)
+    @test !occursin(Colors.hex(Gadfly.dark_theme.default_color), svg_str_light)
 #    @test !occursin("rgba(34,40,48,1)", svg_str_light)
 
 
