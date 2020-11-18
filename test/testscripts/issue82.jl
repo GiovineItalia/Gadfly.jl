10050de7349a8f00918ed781c2cfac7873e54d3a
Milan Bouchet-Valat <nalimilan@club.fr>
2020-11-18 13:55:39 +0100

    Fix test

--- test/testscripts/issue82.jl
+++ test/testscripts/issue82.jl
@@ -1,6 +1,6 @@
-using Gadfly, DataFrames
+using Gadfly, DataFrames, CategoricalArrays
 
 set_default_plot_size(6inch, 3inch)
 
-a = DataFrame(diff = CategoricalArrays.CategoricalArray([1,2,3,3,3,4,3,2]))
+a = DataFrame(diff = CategoricalArray([1,2,3,3,3,4,3,2]))
 plot(a, x="diff", Geom.histogram)
