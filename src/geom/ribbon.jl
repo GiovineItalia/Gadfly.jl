21d8674eaa48d54037ff5ed998141569d39efee0
Mattriks <Mattriks@users.noreply.github.com>
2021-01-19 09:44:44 +1100

    Horizontal or vertical ribbon (#1513)
    * horizontal or vertical ribbon

--- src/geom/ribbon.jl
+++ src/geom/ribbon.jl
@@ -17,11 +17,29 @@ const ribbon = RibbonGeometry
 
 default_statistic(geom::RibbonGeometry) = geom.default_statistic
 
-element_aesthetics(::RibbonGeometry) = [:x, :ymin, :ymax, :color, :linestyle, :alpha]
+element_aesthetics(::RibbonGeometry) = [:x, :ymin, :ymax, :y, :xmin, :xmax, :color, :linestyle, :alpha]
 
 function render(geom::RibbonGeometry, theme::Gadfly.Theme, aes::Gadfly.Aesthetics)
-    Gadfly.assert_aesthetics_defined("Geom.ribbon", aes, :x, :ymin, :ymax)
-    Gadfly.assert_aesthetics_equal_length("Geom.ribbon", aes, :x, :ymin, :ymax)
+    orientation = if isempty(Gadfly.undefined_aesthetics(aes, :x, :ymin, :ymax))
+        :vertical
+    elseif isempty(Gadfly.undefined_aesthetics(aes, :y, :xmin, :xmax))
+        :horizontal
+    else
+        error("For `Geom.ribbon`, define either `x`, `ymin`, `ymax` or `y`, `xmin`, `xmax`")
+    end
+
+    var, minvar, maxvar = if orientation==:vertical
+        :x, :ymin, :ymax
+    else
+        :y, :xmin, :xmax
+    end
+
+    Gadfly.assert_aesthetics_defined("Geom.ribbon", aes, var, minvar, maxvar)
+    Gadfly.assert_aesthetics_equal_length("Geom.ribbon", aes, var, minvar, maxvar)
+
+    vals = getfield(aes, var)
+    maxvals = getfield(aes, maxvar)
+    minvals = getfield(aes, minvar)
 
     default_aes = Gadfly.Aesthetics()
     default_aes.linestyle = [1]
@@ -29,10 +47,10 @@ function render(geom::RibbonGeometry, theme::Gadfly.Theme, aes::Gadfly.Aesthetic
     default_aes.alpha = [1]
     aes = inherit(aes, default_aes)
 
-    aes_x, aes_ymin, aes_ymax, aes_color, aes_linestyle, aes_alpha =
-         concretize(aes.x, aes.ymin, aes.ymax, aes.color, aes.linestyle, aes.alpha)
-    XT, CT, LST, AT = eltype(aes_x), eltype(aes_color), eltype(aes_linestyle), eltype(aes_alpha)
-    YT = eltype(aes_ymin)
+    aes_vals, aes_minvals, aes_maxvals, aes_color, aes_linestyle, aes_alpha =
+         concretize(vals, minvals, maxvals, aes.color, aes.linestyle, aes.alpha)
+    XT, CT, LST, AT = eltype(aes_vals), eltype(aes_color), eltype(aes_linestyle), eltype(aes_alpha)
+    YT = eltype(aes_minvals)
     groups = collect((Tuple{CT, LST, AT}), Compose.cyclezip(aes_color, aes_linestyle, aes_alpha))
     ugroups = unique(groups)
     nugroups = length(ugroups)
@@ -42,17 +60,25 @@ function render(geom::RibbonGeometry, theme::Gadfly.Theme, aes::Gadfly.Aesthetic
 
     max_points = Dict{K, V}(g=>V[] for g in ugroups)
     min_points = Dict{K, V}(g=>V[] for g in ugroups)
-    for (x, ymin, ymax, c, ls, a) in Compose.cyclezip(aes_x, aes_ymin, aes_ymax, aes_color, aes_linestyle, aes_alpha)
-        push!(max_points[(c,ls,a)], (x, ymax))
-        push!(min_points[(c,ls,a)], (x, ymin))
+    orderf = first
+    if orientation==:vertical
+        for (x, ymin, ymax, c, ls, a) in Compose.cyclezip(aes_vals, aes_minvals, aes_maxvals, aes_color, aes_linestyle, aes_alpha)
+            push!(max_points[(c,ls,a)], (x, ymax))
+            push!(min_points[(c,ls,a)], (x, ymin))
+        end
+    else
+        for (x, ymin, ymax, c, ls, a) in Compose.cyclezip(aes_vals, aes_minvals, aes_maxvals, aes_color, aes_linestyle, aes_alpha)
+            push!(max_points[(c,ls,a)], (ymax, x))
+            push!(min_points[(c,ls,a)], (ymin, x))
+        end
+        orderf = last
     end
-
-    for k in keys(max_points)
-        sort!(max_points[k], by=first)
-        sort!(min_points[k], by=first, rev=true)
-    end
-
     kys = keys(max_points)
+    for k in kys
+        sort!(max_points[k], by=orderf)
+        sort!(min_points[k], by=orderf, rev=true)
+    end
+
     polys = [collect(Tuple{XT, YT}, Iterators.flatten((min_points[k], max_points[k]))) for k in kys]
     lines = [collect(Tuple{XT, Union{YT, Missing}}, Iterators.flatten((min_points[k], [(last(min_points[k])[1], missing)], max_points[k]))) for k in kys]
 
--- test/testscripts/ribbon.jl
+++ test/testscripts/ribbon.jl
@@ -1,6 +1,6 @@
 using Gadfly, DataFrames
 
-set_default_plot_size(12inch, 3inch)
+set_default_plot_size(6.6inch, 6.6inch)
 
 xs = 0:0.1:20
 
@@ -8,6 +8,7 @@ df = [DataFrame(x=xs, y=y, ymin=y.-0.5, ymax=y.+0.5, f=f) for (y,f) in zip((cos.
 
 p1 = plot(df[1], x=:x, y=:y, ymin=:ymin, ymax=:ymax, Geom.line, Geom.ribbon)
 p2 = plot(df[1], Geom.ribbon, x=:x, ymin=:ymin, ymax=:ymax, color=[colorant"red"],  alpha=[0.3], Theme(lowlight_color=identity))
-p3 = plot(vcat(df...), x=:x, y=:y, ymin=:ymin, ymax=:ymax, color=:f, Geom.line, Geom.ribbon)
+p3 = plot(df[1], Geom.ribbon, y=:x, xmin=:ymin, xmax=:ymax, color=[colorant"red"],  alpha=[0.3], Theme(lowlight_color=identity))
+p4 = plot(vcat(df...), x=:x, y=:y, ymin=:ymin, ymax=:ymax, color=:f, Geom.line, Geom.ribbon)
 
-hstack(p1, p2, p3)
+gridstack([p1 p2; p3 p4])
