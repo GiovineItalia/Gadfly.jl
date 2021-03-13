12c8ada6a4d334358e0f892b4c4b2a12e56ef393
Rik Huijzer <rikhuijzer@pm.me>
2021-03-13 13:13:06 +0100

    Use faithful dataset and hstack

--- test/testscripts/stat_quantile_bars.jl
+++ test/testscripts/stat_quantile_bars.jl
@@ -2,15 +2,14 @@ using Gadfly, RDatasets, Distributions
 
 set_default_plot_size(6inch, 3inch)
 
-plot(dataset("ggplot2", "diamonds"), x=:Price, Stat.quantile_bars, Geom.segment)
+xs = dataset("datasets", "faithful").Waiting
 
-dist = MixtureModel(Normal, [(0.5, 0.2), (1, 0.1)])
-xs = rand(dist, 10^5)
-p = plot(
-    layer(x=xs, Stat.quantile_bars(quantiles=[0.1, 0.9]), Geom.segment, color=["auto"]),
-    layer(x=xs, Geom.density, color=["auto"]),
-    layer(x=xs, Stat.quantile_bars(bandwidth=0.0003), Geom.segment, color=["bw=0.0003"]),
-    layer(x=xs, Geom.density(bandwidth=0.0003), color=["bw=0.0003"]),
-    Scale.color_discrete_manual("orange", "green"),
-    Guide.colorkey(title="bandwidth")
-);
+quantiles = [0.1, 0.9]
+p1 = plot(x=xs, Geom.density, Guide.title("quantiles=$quantiles"),
+        layer(x=xs, Stat.quantile_bars(quantiles=quantiles), Geom.segment))
+
+bandwidth = 0.6
+p2 = plot(x=xs, Stat.density(bandwidth=bandwidth), Geom.line, Guide.title("bandwidth=$bandwidth"),
+    layer(x=xs, Stat.quantile_bars(bandwidth=bandwidth), Geom.segment))
+
+p = hstack(p1, p2)
