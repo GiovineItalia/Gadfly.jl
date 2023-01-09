0079d079dfb82217f4f944f0dfd3a40ca20e9655
Mattriks <Mattriks@users.noreply.github.com>
2023-01-09 13:43:11 +1100

    ribbon with group aesthetic

--- src/geom/ribbon.jl
+++ src/geom/ribbon.jl
@@ -17,7 +17,7 @@ const ribbon = RibbonGeometry
 
 default_statistic(geom::RibbonGeometry) = geom.default_statistic
 
-element_aesthetics(::RibbonGeometry) = [:x, :ymin, :ymax, :y, :xmin, :xmax, :color, :linestyle, :alpha]
+element_aesthetics(::RibbonGeometry) = [:x, :ymin, :ymax, :y, :xmin, :xmax, :color, :linestyle, :alpha, :group]
 
 function render(geom::RibbonGeometry, theme::Gadfly.Theme, aes::Gadfly.Aesthetics)
     orientation = if isempty(Gadfly.undefined_aesthetics(aes, :x, :ymin, :ymax))
@@ -35,7 +35,7 @@ function render(geom::RibbonGeometry, theme::Gadfly.Theme, aes::Gadfly.Aesthetic
     end
 
     Gadfly.assert_aesthetics_defined("Geom.ribbon", aes, var, minvar, maxvar)
-    Gadfly.assert_aesthetics_equal_length("Geom.ribbon", aes, var, minvar, maxvar)
+    Gadfly.assert_aesthetics_equal_length("Geom.ribbon", aes, var, maxvar)
 
     vals = getfield(aes, var)
     maxvals = getfield(aes, maxvar)
@@ -45,31 +45,32 @@ function render(geom::RibbonGeometry, theme::Gadfly.Theme, aes::Gadfly.Aesthetic
     default_aes.linestyle = [1]
     default_aes.color = Colorant[theme.default_color]
     default_aes.alpha = [1]
+    default_aes.group = IndirectArray([1])
     aes = inherit(aes, default_aes)
 
-    aes_vals, aes_minvals, aes_maxvals, aes_color, aes_linestyle, aes_alpha =
-         concretize(vals, minvals, maxvals, aes.color, aes.linestyle, aes.alpha)
-    XT, CT, LST, AT = eltype(aes_vals), eltype(aes_color), eltype(aes_linestyle), eltype(aes_alpha)
-    YT = eltype(aes_minvals)
-    groups = collect((Tuple{CT, LST, AT}), Compose.cyclezip(aes_color, aes_linestyle, aes_alpha))
+    aes_vals, aes_minvals, aes_maxvals, aes_color, aes_linestyle, aes_alpha, aes_group =
+         concretize(vals, minvals, maxvals, aes.color, aes.linestyle, aes.alpha, aes.group)
+    CT, LST, AT, GT = eltype(aes_color), eltype(aes_linestyle), eltype(aes_alpha), eltype(aes_group)
+    XT, YT = eltype(aes_vals), eltype(aes_minvals)
+    groups = collect((Tuple{CT, LST, AT, GT}), Compose.cyclezip(aes_color, aes_linestyle, aes_alpha, aes_group))
     ugroups = unique(groups)
     nugroups = length(ugroups)
     
     V = Vector{Tuple{XT, YT}}
-    K = Tuple{CT, LST, AT}
+    K = Tuple{CT, LST, AT, GT}
 
     max_points = Dict{K, V}(g=>V[] for g in ugroups)
     min_points = Dict{K, V}(g=>V[] for g in ugroups)
     orderf = first
     if orientation==:vertical
-        for (x, ymin, ymax, c, ls, a) in Compose.cyclezip(aes_vals, aes_minvals, aes_maxvals, aes_color, aes_linestyle, aes_alpha)
-            push!(max_points[(c,ls,a)], (x, ymax))
-            push!(min_points[(c,ls,a)], (x, ymin))
+        for (x, ymin, ymax, c, ls, a, g) in Compose.cyclezip(aes_vals, aes_minvals, aes_maxvals, aes_color, aes_linestyle, aes_alpha, aes_group)
+            push!(max_points[(c,ls,a,g)], (x, ymax))
+            push!(min_points[(c,ls,a,g)], (x, ymin))
         end
     else
-        for (x, ymin, ymax, c, ls, a) in Compose.cyclezip(aes_vals, aes_minvals, aes_maxvals, aes_color, aes_linestyle, aes_alpha)
-            push!(max_points[(c,ls,a)], (ymax, x))
-            push!(min_points[(c,ls,a)], (ymin, x))
+        for (x, ymin, ymax, c, ls, a, g) in Compose.cyclezip(aes_vals, aes_minvals, aes_maxvals, aes_color, aes_linestyle, aes_alpha, aes_group)
+            push!(max_points[(c,ls,a,g)], (ymax, x))
+            push!(min_points[(c,ls,a,g)], (ymin, x))
         end
         orderf = last
     end
@@ -90,7 +91,7 @@ function render(geom::RibbonGeometry, theme::Gadfly.Theme, aes::Gadfly.Aesthetic
     alpha_discrete  = AT <: Int
     linestyle_discrete = LST <: Int
 
-    for (i, (c,ls,a)) in enumerate(kys)
+    for (i, (c,ls,a,g)) in enumerate(kys)
         classes[i] = svg_color_class_from_label(aes.color_label([c])[1])
         colors[i] = parse_colorant(theme.lowlight_color(c))
         linestyles[i] = linestyle_discrete ? get_stroke_vector(theme.line_style[mod1(ls, linestyle_palette_length)]) : get_stroke_vector(ls)
